934200c4c6ef7cc8a300a28308d07148a0142128
jabapo <jabapo@umich.edu>
2022-08-19 10:28:48 -0400

    functionize create item button, current coords for later use, change useEffect to use more of the onsnapshot, unsubscribe on unmount

--- src/Map.js
+++ src/Map.js
@@ -3,15 +3,16 @@ import { SimpleGrid,
          Button,
          Grid,
          Paper,
+         Group,
          Card,
          Image,
-         Group,
          Text,
          Badge,
-         Center
+         Center,
         } from '@mantine/core';
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs, addDoc, docRef, query, onSnapshot } from 'firebase/firestore';
+import { toContainElement } from '@testing-library/jest-dom/dist/matchers';
 const firebaseConfig = {
   apiKey: "AIzaSyDcsr-FDygOtD2VHPwqNY9wKmU_lMPIucQ",
   authDomain: "sanvas-5ba8d.firebaseapp.com",
@@ -26,6 +27,22 @@ const db = getFirestore(app);
 
 function Map(){
     // array should be 10x10 (0-9)
+    function createMapButton(row_idx,col_idx,cell) {
+        let coords = '' + row_idx + col_idx;
+        let obj = {
+            coords: coords,
+            color: cell.displayName ? "red" : "blue",
+        }
+        return (<Button
+            size='sm'
+            color={obj.color} 
+            key={obj.coords}
+            value={coords}
+            onClick={clickHandler}
+            variant="filled">
+        </Button>)
+    }
+
     const placeholder_item = {
         description:'',
         imagePNG:'',
@@ -33,92 +50,105 @@ function Map(){
         displayName:''
     }
     const map_array = new Array(10).fill(placeholder_item).map(() => new Array(10).fill(placeholder_item));
+    const [currentCoords,setCurrentCoords] = useState({x:-1,y:-1});
     const [itemList,setItemList] = useState([]);
     const [currentItem,setCurrentItem] = useState({});
-    const [displayImage,setDisplayImage] = useState(<div>1</div>);
-    const hoverEffect = (value) => {
-        //console.log(ref)
-    }
+    const [displayImage,setDisplayImage] = useState(null);
 
     const clickHandler = (event) =>{
         const coords = event.currentTarget.value;
         const x = coords[0];
         const y = coords[1];
+        setCurrentCoords({x,y})
         setCurrentItem(map_array[x][y]);
     }
 
     useEffect(()=>{
         // pass the currentItem props to the thing
+        let noitem = "https://media.istockphoto.com/photos/empty-pedestal-inside-exhibition-gallery-picture-id1271894342?k=20&m=1271894342&s=170667a&w=0&h=4Cy45Werofk-XvvjgxU_dYgoQgXRawE_TEEn3BsVbx0=";
+        let tmp = {
+            imagePNG:"https://consciouscat.net/wp-content/uploads/2012/11/cat-immune-system-e1587891908928.jpg",
+            artName:"cute cat",
+            displayName:"jeremy",
+            timestamp:"earlier today",
+            description:"select any box to see art work!"
+        };
+        let text;
+        if (currentCoords.x !== -1 && currentCoords.y !== -1 && currentItem.displayName !== ''){
+            tmp = currentItem;
+        }
+        text = `${tmp.artName} by ${tmp.displayName}`;
+        if (currentItem.displayName === ''){
+            text = "nothing is here...";
+            tmp.imagePNG = noitem;
+        }
         setDisplayImage(<Card shadow="sm" p="lg" radius="md" withBorder>
                             <Card.Section>
                                 <Image
-                                src={currentItem.imagePNG}
+                                src={tmp.imagePNG}
                                 height="100%"
-                                alt={currentItem.artName}
+                                alt={tmp.artName}
                                 />
                             </Card.Section>
                             <Group position="apart" mt="md" mb="xs">
-                                <Text weight={500}>{currentItem.displayName}</Text>
+                                <Text weight={500}>
+                                    {text}
+                                </Text>
                                 <Badge color="pink" variant="light">
-                                Timestamp here
+                                {tmp.timestamp}
                                 </Badge>
                             </Group>
                             <Text size="sm" color="dimmed">
-                                {currentItem.description}
+                                {tmp.description}
                             </Text>
-
+                            {currentCoords.x !== -1 ?
                             <Button variant="light" color="blue" fullWidth mt="md" radius="md">
-                                place your art here?
+                                item located at x:{currentCoords.x} y:{currentCoords.y}
                             </Button>
+                            :
+                            <Button variant="light" color="blue" fullWidth mt="md" radius="md" disabled>
+                                select a box
+                            </Button>
+                            }
                         </Card>);
-    },[currentItem])
+    },[currentItem,currentCoords])
 
     // initialize the grid
     // set up the listener
     useEffect(()=>{
+        let unsubscribe;
         async function getMap(db){
-            const unsubscribe = onSnapshot(collection(db, "map"),(querySnapshot)=>{
-                querySnapshot.forEach((doc) =>{
-                    const x = doc.id[0];
-                    const y = doc.id[2];
-                    if (doc.data().displayName){
-                        map_array[x][y] = doc.data();
+            // set up the listener
+            unsubscribe = onSnapshot(collection(db, "map"),(querySnapshot)=>{
+                querySnapshot.docChanges().forEach((change)=>{
+                    if (change.type === "added") {
+                        const x = change.doc.id[0];
+                        const y = change.doc.id[2];
+                        // update the item list at that coordinate
+                        if (change.doc.data().displayName){
+                            map_array[x][y] = change.doc.data();
+                            console.log(x,y," added item @ map array data:",map_array[x][y]);
+                            createMapButton(x,y,change.doc.data());
+                        }
                     }
                 });
+                // start the initial item list
                 let item_list;
                 item_list = map_array.map((rows,row_idx)=>{
                     let tmp = [];
                     rows.map((cell,col_idx)=>{
-                        let coords = '' + row_idx + col_idx;
-                        let obj = {
-                            coords: coords,
-                            color: cell.displayName ? "red" : "blue",
-                        }
-                        tmp.push(
-                        <Button
-                            size='sm'
-                            color={obj.color} 
-                            key={obj.coords}
-                            value={coords}
-                            onClick={clickHandler}
-                            onMouseOver={hoverEffect}
-                            variant="outline">
-                        </Button>
-                        );
+                        tmp.push(createMapButton(row_idx,col_idx,cell));
                     })
                     return tmp;
                 })
                 setItemList(item_list);
             });
-            
         };
         getMap(db);
-
-        // initialize the grid
-    });
+        return () => unsubscribe();
+    },[]);
 
     // if change in the snapshot, update that button
-
     return(
         // first make an empty 10x10 grid
         <>
